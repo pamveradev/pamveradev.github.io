ebacdd8927607e6e9fe28f58f122a56d34da12aa
Pam <hpdeme@gmail.com>
2026-03-03 18:36:08 -0300

    upd git

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pamveradev.github.io/', // Cambiar a '/portafolio/' si el repo no es username.github.io
+  base: '/', // Cambiar a '/portafolio/' si el repo no es username.github.io
 })
